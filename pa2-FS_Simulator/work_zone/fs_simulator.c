27f08f37e0133baddc5648b8f0a002d168b058d6
Jeremy <jeremyhuangofficial@gmail.com>
2024-06-21 02:20:48 +0000

    added function called load_inodes; tested reading current # of inodes in program

--- pa2-FS_Simulator/work_zone/fs_simulator.c
+++ pa2-FS_Simulator/work_zone/fs_simulator.c
@@ -19,8 +19,7 @@ typedef struct {
 	char name[FNAME_SIZE + 1];
 } Directory;
 
-Inode inodes_list[MAX_INODES];
-
+// Function parses and verifies program arguments for correct startup
 void parse_args(int argc, char* argv1)
 {
 	// Verify number of parameters from input
@@ -39,8 +38,59 @@ void parse_args(int argc, char* argv1)
 	}
 }
 
+// Function reads the "inodes_list" file and returns the number of possible entries left
+int read_inodes_list(Inode* inodes_list)
+{
+	// Load the inodes_list file
+	FILE* inodes_list_file = fopen("inodes_list", "r");
+	char buff[5];
+	int num_inodes = MIN_INODES;
+	int inode_size = sizeof(uint32_t) + sizeof(char);
+	int one_inode = 1;
+
+	// Read 1 inode (5 bytes) from the inodes_list and store this in buff until we are at the end of "inodes_list"
+		// fread(*buffer, size of n, n elements to read, *file_stream);
+	while (num_inodes < MAX_INODES && fread(buff, inode_size, one_inode, inodes_list_file) != 0)
+	{
+		inodes_list[num_inodes].index = buff[0];
+		inodes_list[num_inodes].type  = buff[4];
+		num_inodes++;
+	}
+	fclose(inodes_list_file);
+
+	// Note: num_inodes is 1 behind, but if it is bigger or equal to the max number of possible inodes there's an issue
+	if (num_inodes >= MAX_INODES - 1)
+	{
+		return -1;
+	}
+
+	// Initialize the remaining list for reminder that:
+	// 	-1 is nonexistent inode,
+	//	'\0' is nonexistent type
+	for (int i = num_inodes; i < MAX_INODES; i++)
+	{
+		inodes_list[i].index = -1;
+		inodes_list[i].type  = '\0';
+	}
+
+	// Return the current size of our array / number of current inodes
+	return num_inodes;
+}
+
 int main(int argc, char* argv[])
 {
 	// Check if the arguments are valid first
 	parse_args(argc, argv[1]);
+
+	// Array holding 1024 possible contents from the file "inodes_list"
+	Inode inodes_list_entries[MAX_INODES];
+
+	// Read the "inodes_list" file and get the current number of inodes
+	int cur_inodes = read_inodes_list(inodes_list_entries);
+
+	int starting_inodes = cur_inodes;
+	int rem_inodes = MAX_INODES - cur_inodes;
+
+	printf("cur_inodes-%d\n", cur_inodes);
+	printf("rem_inodes-%d\n", rem_inodes);
 }
